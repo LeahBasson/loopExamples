b07e8e0e02e8f24b7b0b8c868693ca420ce57ef1
LeahBasson <leahbasson54@gmail.com>
2024-09-18 08:42:07 +0200

    loop examples

---
+++ script/code.js
@@ -0,0 +1,54 @@
+// for (let i=1 ; i<10 ; i++)
+// {
+//     console.log(i);
+// }
+
+let numbers=[9,3,11,4,7,12]
+// for (let i=0; i<numbers.length; i++)
+// {
+//     console.log(numbers[i]);
+// }
+
+// Odd numbers
+// if (numbers[i] %  2 !== 0){
+//     console.log(numbers[i]);
+// }
+
+// Even numbers
+// if (numbers[i] %  2 == 0){
+//     console.log(numbers[i]);
+// }
+
+// for (let i=0; i <numbers.length; i +=2){
+//     if(numbers[i] == 11){
+//         continue
+//     }
+//     console.log(numbers[i]);
+// }
+
+// let cnt = 0
+// while (cnt < numbers.length){
+//     console.log(numbers[cnt]);
+//     cnt++
+// }
+
+// let cnt = 0;
+// while(cnt < numbers.length) {
+//     console.log(`${cnt} -> ${numbers[cnt]}`);
+//     cnt ++
+// }
+
+// for (let i=0; i< numbers.length; i++) {
+//     console.log(`${numbers[i]} is at position ${i}`);
+// }
+
+for (let n in numbers){
+    console.log(numbers[n]);
+}
+
+
+
+
+
+
+
